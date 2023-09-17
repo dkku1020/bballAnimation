c1d7d37f5d5ef5e6c2db64bd1c11a4ee6b525b38
DanielKu <kusame25@gmail.com>
2023-09-16 21:44:30 -0400

    freeze frame when holding click

--- bball.js
+++ bball.js
@@ -11,8 +11,8 @@ export class BBall {
     this.ballWidthHalf = this.ballWidth / 2;
 
     // Position & Velocity
-    this.x = 500;
-    this.y = 500;
+    this.x = 200;
+    this.y = 200;
     this.vx = 0;
     this.vy = 0;
 
@@ -22,27 +22,45 @@ export class BBall {
 
     // Controls
     this.dribbleDirection = 1;
+    this.isAiming = false;
     
     // Physics
-    this.gravity = 0.5;
+    this.gravity = 0.8;
     this.bounceFactor = -0.8;
     this.friction = 0.98;
 
     document.addEventListener('click', this.handleDribble.bind(this));
+    document.addEventListener('mousedown', this.aim.bind(this));
+    document.addEventListener('mouseup', this.stopaim.bind(this));
+
+  }
+
+  aim() {
+    this.isAiming = true;
+    this.vx = 0;
+    this.vy = 0;
+    this.angularVelocity = 0;
+  }
+
+  stopaim() {
+    this.isAiming = false;
   }
 
   handleDribble() {
-    this.vy = 15;
+    if (this.isAiming) return;
+    this.vy = 18;
 
     let strength = 10;
     let crossOver = strength * this.dribbleDirection;
-    this.vx += crossOver;
+    this.vx = crossOver;
     this.dribbleDirection *= -1;
   }
 
   animate(ctx) {
     // Gravity
-    this.vy += this.gravity;
+    if (!this.isAiming) {
+      this.vy += this.gravity;
+    }
 
     // Horizontal Friction (only when ball touches ground)
     if (this.y + this.ballHeight >= this.stageHeight) {
